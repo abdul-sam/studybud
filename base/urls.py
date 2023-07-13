f6233a40c0c6e4e10d2bf2b635a9d32e1d38d1c6
abdul-sam <abdulsamad.3939@gmail.com>
2023-07-13 10:32:11 +0500

    Complete login and logout functionality

--- base/urls.py
+++ base/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     re_path(r'^login/$', views.login_page, name='login'),
+    re_path(r'^logout/$', views.logout_user, name='logout'),
     re_path(r'^room/(?P<pk>\d+)/$', views.room, name='room'),
     re_path(r'^rooms/new/$', views.new_room, name='new_room'),
     re_path(r'^rooms/(?P<pk>\d+)/edit$', views.update_room, name='edit_room'),
